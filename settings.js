8edb082772c8de01080a5027827b0ed691e33107
あ ┊ 𝐂𝐫𝐚𝐱𝐤𝐞𝐫 <guzmanjuliansebastian@gmail.com>
2025-08-08 18:58:07 -0300

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/CH3hrd4ovGHIsFjj8dWGrf'
+global.group = 'https://chat.whatsapp.com/Lp9gBDfaRIp6W9sEbpgjEF'
 global.group2 = 'https://chat.whatsapp.com/EXVs651isvlB7MuHiwcLl8'
 global.group3 = 'https://chat.whatsapp.com/CKF7jbLN877GLxoIJdbhq1'
 global.canal = 'https://whatsapp.com/channel/0029VbAh8QO2Jl8CHj5ojQ0R'
