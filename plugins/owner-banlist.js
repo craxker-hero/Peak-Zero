3d37e0aa46b066bf06d2f5ab5860e60607c7a03f
あ ┊ 𝐂𝐫𝐚𝐱𝐤𝐞𝐫 <guzmanjuliansebastian@gmail.com>
2025-08-09 21:30:52 -0300

    Actualizar owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -1,20 +1,33 @@
 let handler = async (m, { conn, isOwner }) => {
-let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
-let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
-let caption = `
-╭─「 *Usuarios Baneados* 」
-║❥ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
-║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
-╰────
+  let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
+  let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
+  
+  let userList = users.length ? 
+    users.map(([jid], i) => `❐ ${isOwner ? '@' + jid.split('@')[0] : jid}`).join('\n') : 
+    '❀ No hay usuarios baneados'
+  
+  let chatList = chats.length ? 
+    chats.map(([jid], i) => `❐ ${isOwner ? '@' + jid.split('@')[0] : jid}`).join('\n') : 
+    '❀ No hay chats baneados'
 
-╭─「 *Chats Baneados* 」
-║❥ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
-║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
-╰────
+  let caption = `
+❐　「 *Usuarios Baneados* 」
+✦» *Total* : ${users.length}
+${userList}
+
+
+❐　「 *Chats Baneados* 」
+✦» *Total* : ${chats.length}
+${chatList}
+
 `.trim()
-m.reply(caption, null, {mentions: conn.parseMention(caption)})}
+
+  await m.reply(caption, null, { mentions: conn.parseMention(caption) })
+}
+
 handler.help = ['banlist']
 handler.tags = ['owner']
 handler.command = /^banlist(ned)?|ban(ned)?list|daftarban(ned)?$/i
 handler.rowner = true
+
 export default handler
