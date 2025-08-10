632cdec093d3a1a11b6e2c6850c1013078936b86
あ ┊ 𝐂𝐫𝐚𝐱𝐤𝐞𝐫 <guzmanjuliansebastian@gmail.com>
2025-08-10 08:57:55 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'
 import fetch from 'node-fetch'
-import levelHandler from './plugins/rg-levelup.js'
+import levelHandler from './plugins/rpg-levelup.js'
 
 const { proto } = (await import('@whiskeysockets/baileys')).default
 const isNumber = x => typeof x === 'number' && !isNaN(x)
